bf24a4979c3f254851fda7651eb897641d5e2279
Todd Hassell <toddhassell@todds-mac-mini.local>
2016-02-18 19:20:42 +0900

    Initial Commit

---
+++ pokemon-pokedex/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pokemon-pokedex
+//
+//  Created by Todd Hassell on 2/18/16.
+//  Copyright © 2016 Todd Hassell. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
